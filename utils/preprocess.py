93c245da35ee2f09d28dfb48c3d771626019198e
An, Dinh Le Binh <dinhlebinhan2005@gmail.com>
2025-04-18 23:09:58 +0700

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,12 @@
-def preprocess_image():
-    pass
+import cv2
+import torch
+import numpy as np
+import torch.nn.functional as F
+
+def preprocess_image(img, img_size):
+    """Preprocess image: resize and normalize."""
+    img = cv2.resize(img, img_size)
+    return img
 
 def augment_data():
     pass
