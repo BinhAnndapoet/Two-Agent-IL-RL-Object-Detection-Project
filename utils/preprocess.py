bc682e503d9b8f4ec8263ec641fd37e724008510
KhoiBui16 <khoib1601@gmail.com>
2025-04-18 23:13:07 +0700

    add comment for augment_data function

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,14 @@
-def preprocess_image():
+
+
+def preprocess_image(img, img_size):
     pass
 
-def augment_data():
+def augment_data(img_tensor, bboxes, labels):
+    """  
+        - img_tensor: [C, H, W]
+        - bboxes : [N, 4] (x, y, w, h) - tensor([[]]) : 2 chiều
+        - labels: [N]
+    """
+    
+    
     pass
