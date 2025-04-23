331d1c66435fbfa34c138f3284143672c8dacf92
An, Dinh Le Binh <dinhlebinhan2005@gmail.com>
2025-04-23 23:47:22 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,15 +9,141 @@ class Replay_Buffer():
             batchsize: The batch size used for training
     """
     def __init__(self):
-      pass
+        pass
 
     def append(self):
-      pass
+        """ Appends a transition to the replay buffer """
+        pass
 
     def sample_batch(self):
-      pass
+        """ Samples a batch of transitions from the replay buffer """
+        pass
+        
     def initialize(self):
-      pass
+        """ Initializes the replay buffer by sampling transitions from the environment """
+        pass
 
 def iou():
-  pass
+        """
+            Calculating the IoU between two bounding boxes.
+
+            Formula:
+                IoU(b, g) = area(b ∩ g) / area(b U g)
+
+            Args:
+                bbox1: The first bounding box.
+                target_bbox: The second bounding box.
+
+            Returns:
+                The IoU between the two bounding boxes.
+
+        """
+    pass
+
+def recall():
+    """
+        Calculating the recall between two bounding boxes.
+
+        Formula:
+            Recall(b, g) = area(b ∩ g) / area(g)
+
+        Args:
+            bbox: The first bounding box.
+            target_bbox: The second bounding box.
+
+        Returns:
+            The recall between the two bounding boxes.
+    """
+    pass
+
+def calculate_best_iou():
+    """
+        Calculating the best IoU between the bounding boxes and the ground truth boxes.
+
+        Args:
+            bounding_boxes: The predicted bounding boxes.
+            gt_boxes: The ground truth bounding boxes.
+
+        Returns:
+            The best IoU between the bounding boxes and the ground truth boxes.
+    """
+    pass
+
+def calculate_best_recall():
+    """
+        Calculating the best recall between the bounding boxes and the ground truth boxes.
+
+        Args:
+            bounding_boxes: The predicted bounding boxes.
+            gt_boxes: The ground truth bounding boxes.
+
+        Returns:
+            The best recall between the bounding boxes and the ground truth boxes.
+    """
+    pass
+
+def calculate_precision_recall():
+    """
+        Calculating the precision and recall using the Intersection over Union (IoU) and according to the threshold between the ground truths and the predictions.
+
+        Args:
+            bounding_boxes: The predicted bounding boxes.
+            gt_boxes: The ground truth bounding boxes.
+            ovthresh: The IoU threshold.
+
+        Returns:
+            precision (tp / (tp + fp))
+            recall (tp / (tp + fn))
+            f1 score (2 * (precision * recall) / (precision + recall))
+            average IoU (sum of IoUs / number of bounding boxes)
+            average precision (sum of precisions / number of bounding boxes)
+    """
+    pass
+
+def voc_ap():
+    """
+    Calculating the Average Precision (AP) and Recall.
+
+        Args:
+            rec: Array of recall values.
+            prec: Array of precision values.
+            voc2007: Boolean flag indicating whether to use the method recommended by the PASCAL VOC 2007 paper (11-point method).
+
+        Returns:
+            The average precision (AP).
+
+        More information:
+        - If voc2007 is True, then the method recommended by the PASCAL VOC 2007 paper (11-point method) is used.
+        - If voc2007 is False, then the method recommended by the PASCAL VOC 2010 paper is used.
+    """
+    pass
+
+def calculate_class_detection_metrics(current_class, bounding_boxes, gt_boxes, ovthresh):
+    """
+        Calculating the VOC detection metric.
+
+        Args:
+            current_class: The current class/label.
+            bounding_boxes: The predicted bounding boxes.
+            gt_boxes: The ground truth bounding boxes.
+            ovthresh: The IoU threshold.
+
+        Returns:
+            The average precision.
+    """
+    pass
+
+def calculate_detection_metrics():#list(np.arange(0.5, 0.95, 0.05))):
+    """
+    Calculating the detection metrics for all the classes.
+
+        Args:
+            results_path: Path to the directory containing detection results.
+            threshold_list: List of IoU thresholds to evaluate detection metrics.
+
+        Returns:
+            dfs: An array of pandas dataframes containing the detection metrics for each class at given IoU thresholds.
+            mAps: The mean average precision for each class at given IoU thresholds.
+            pre_rec_f1: Precision, recall, and F1-score for each class at given IoU thresholds.
+    """
+    pass
